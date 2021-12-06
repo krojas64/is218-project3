cdf8f4cd9a9182628f3415a423a316b7bd62f1db
Karel Rojas <karelrojas64@gmail.com>
2021-12-06 09:51:44 -0500

    Files for CSV input

--- csv/main.py
+++ csv/main.py
@@ -1,6 +1,12 @@
 import pandas as pd
 import numpy as np
-from csv.abspath import absolutepath
+from abspath import absolutepath
+from calc.operations.addition import Addition
 
-data = pd.read_csv(absolutepath("input.csv"))
 
+data = pd.read_csv(absolutepath("input/input.csv"))
+
+for idx in data.index:
+    numbers = (data['First'][idx], data['Second'][idx])
+    addition = Addition(numbers)
+    assert addition.get_result() == data['Sum'][idx]
